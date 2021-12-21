3e1acec3282aea8e0c525cc1adc7f00a8ae999ac
Eli Miller <elimiller22@gmail.com>
2021-12-20 23:08:15 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  // put your setup code here, to run once: Test comment
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
